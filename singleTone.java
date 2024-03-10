6ad1da510699e3e7ff4b77afaee06080c99e7be1
Md. Shaeakh Ahmed Chowdhury <102303488+shaeakh@users.noreply.github.com>
2024-03-10 12:07:18 +0600

    Create singleTone.java

---
+++ singleTone.java
@@ -0,0 +1,33 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class a {
+    private int id ;
+    private String s; 
+    a(int id, String s){
+        this.id = id; 
+        this.s = s;
+    }
+    public void cng(int id,String s){
+        this.id = id; 
+        this.s = s;
+    }
+    public void show(){
+        System.out.println("id "+id+" name "+s);
+    }
+}
+
+
+class HelloWorld {
+    public static void main(String[] args) {
+        a A = new a(5,"A");
+        A.show();
+        a B = new a(10,"B");
+        B.show();
+        
+        A = B;
+        A.show();
+        B.cng(1,"C");
+        A.show();
+        
+    }
+}
